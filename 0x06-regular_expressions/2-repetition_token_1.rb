19cd577b9328fa9efca57eb14f32c6f5b9e9bcf1
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-01-30 11:25:19 -0800

    bash3

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 
-puts ARGV[0].scan(/hb?t?n/).join
+argument = ARGV[0].scan(/hb?t?n/).join
+puts argument
