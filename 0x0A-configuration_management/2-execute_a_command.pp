e0214400c8ed6ef7822ee0535acb1f331afd98e4
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-22 15:03:15 -0700

    pppp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, creating a manifest that kills a process named 'killmenow'
 
-exec { 'killmenow':
+exec { 'pkill':
   command     => '/usr/bin/pkill -f killmenow',
   path        => '/usr/bin',
   onlyif      => '/usr/bin/pgrep -f killmenow',
