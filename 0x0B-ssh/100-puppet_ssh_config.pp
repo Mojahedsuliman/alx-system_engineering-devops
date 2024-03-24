13c42d91c05ea9608cb16b5c0f8bb25a64930e6c
mojahedsuliman@github.com <mojahedmahjub@gmail.com>
2024-03-24 15:55:38 -0700

    fourth

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,22 @@
+# Clients config file
+
+$ssh_config_file = '/etc/ssh/ssh_config'
+
+# Ensure SSH client configuration file exists
+file { $ssh_config_file:
+  ensure => file,
+}
+
+file_line { 'Declare identity file':
+  path    => $ssh_config_file,
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#?\s*IdentityFile\s+.*',
+  ensure  => present,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => $ssh_config_file,
+  line    => 'PasswordAuthentication no',
+  match   => '^#?\s*PasswordAuthentication\s+.*',
+  ensure  => present,
+}
